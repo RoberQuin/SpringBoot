ac3a7b7275955ab050019f75b2bd18527b89e218
Roberto <quinonesroberto92@gmail.com>
2023-05-18 10:09:51 -0600

    Agregado de funcionalidad put y delete para médico

--- src/main/java/med/voll/api/medico/Medico.java
+++ src/main/java/med/voll/api/medico/Medico.java
@@ -15,32 +15,50 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import med.voll.api.direccion.Direccion;
 
-@Table(name="medicos")
-@Entity(name="Medico")
+@Table(name = "medicos")
+@Entity(name = "Medico")
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-@EqualsAndHashCode(of="id")
+@EqualsAndHashCode(of = "id")
 public @Data class Medico {
-	
+
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	private String nombre; 
-	private String email; 
+	private String nombre;
+	private String email;
 	private String telefono;
-	private String documento; 
+	private String documento;
+	private Boolean activo;
 	@Enumerated(EnumType.STRING)
 	private Especialidad especialidad;
 	@Embedded
 	private Direccion direccion;
-	
+
 	public Medico(DatosRegistroMedico datosRegistroMedico) {
-		this.nombre=datosRegistroMedico.nombre();
-		this.email=datosRegistroMedico.email();
-		this.telefono=datosRegistroMedico.telefono();
-		this.documento=datosRegistroMedico.documento();
-		this.especialidad=datosRegistroMedico.especialidad();
-		this.direccion=new Direccion(datosRegistroMedico.direccion());
+		this.activo=true;
+		this.nombre = datosRegistroMedico.nombre();
+		this.email = datosRegistroMedico.email();
+		this.telefono = datosRegistroMedico.telefono();
+		this.documento = datosRegistroMedico.documento();
+		this.especialidad = datosRegistroMedico.especialidad();
+		this.direccion = new Direccion(datosRegistroMedico.direccion());
+	}
+
+	public void actualizarDatos(DatosActualizarMedico datosActualizarMedico) {
+		if (datosActualizarMedico.nombre() != null) {
+			this.nombre = datosActualizarMedico.nombre();
+		}
+		if (datosActualizarMedico.documento() != null) {
+			this.documento = datosActualizarMedico.documento();
+		}
+		if (datosActualizarMedico.direccion() != null) {
+			this.direccion = direccion.actualizarDatos(datosActualizarMedico.direccion());
+		}
+	}
+
+	public void desactivarMedico() {
+		this.activo=false;
 	}
 }
