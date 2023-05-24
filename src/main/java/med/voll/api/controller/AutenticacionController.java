f372939a78977bd939f58d061fc52fa59e474b8c
Roberto <quinonesroberto92@gmail.com>
2023-05-23 22:38:01 -0600

    Implementación de auth0-jwt para el login del proyecto

--- src/main/java/med/voll/api/controller/AutenticacionController.java
+++ src/main/java/med/voll/api/controller/AutenticacionController.java
@@ -12,18 +12,24 @@ import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.validation.Valid;
 import med.voll.api.domain.usuarios.DatosAutenticacionUsuario;
+import med.voll.api.domain.usuarios.Usuario;
+import med.voll.api.infra.security.DatosJWTToken;
+import med.voll.api.infra.security.TokenService;
 
 @RestController
 @RequestMapping("/login")
 public class AutenticacionController {
 	@Autowired
 	private AuthenticationManager authenticationManager;
+	@Autowired
+	private TokenService tokenService;
 	
 	@PostMapping
 	public ResponseEntity autenticarUsuario(@RequestBody @Valid DatosAutenticacionUsuario datosAutenticacionUsuario) {
-		Authentication token=new UsernamePasswordAuthenticationToken(datosAutenticacionUsuario.login(), 
+		Authentication Authenticationtoken=new UsernamePasswordAuthenticationToken(datosAutenticacionUsuario.login(), 
 				datosAutenticacionUsuario.clave());
-		authenticationManager.authenticate(token);
-		return ResponseEntity.ok().build();
+		var usuarioAutenticado=authenticationManager.authenticate(Authenticationtoken);
+		var JWTtoken=tokenService.generarToken((Usuario) usuarioAutenticado.getPrincipal());
+		return ResponseEntity.ok(new DatosJWTToken(JWTtoken));
 	}
 }
